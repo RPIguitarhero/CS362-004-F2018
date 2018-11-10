45a60cc36ef30fb2d79b855969c1624656eec070
RPIguitarhero <38031484+RPIguitarhero@users.noreply.github.com>
2018-11-10 19:57:16 +0800

    initial commit
    based on my assignment submission, which has refractured codes and deliberately introduced bugs in 3 card functions.
    The based branch:kangka-assignment-2

---
+++ projects/kangka/dominion/randomtestadventurer.c
@@ -0,0 +1,34 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+printf("now random testing adventurer \n");
+struct gameState* g;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
